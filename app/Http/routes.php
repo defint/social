d04f62b7f5ee3ed289aaf79318fd581539cad6cc
Matrenin Anton <matrenin@ukr.net>
2015-10-24 16:55:17 +0300

    Alerts

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,3 +4,8 @@ Route::get('/',[
     'uses' => '\Social\Http\Controllers\HomeController@index',
     'as' => 'home'
 ]);
+
+Route::get('/alert',function()
+{
+    return redirect()->route('home')->with('info','Welcome!');
+});
